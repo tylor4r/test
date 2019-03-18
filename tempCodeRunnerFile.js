4d67cca7f17c760950a63645a79a755e3f2a0237
Dima <Tylor4r@gmail.com>
2019-03-18 21:45:24 +0300

    Завтра надо вернуться к заданию домашнему

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,8 +1,5 @@
-let persone = {
-    name:'Dima',
-    age:27,
-    isMarried:false
-};
-console.log(persone["name"]);
-let arr = ['plum.png','orange.jpg','apple.bmp'];
-console.log(arr[0]);
+let num = 50;
+while (num < 55){
+    console.log(num);
+    num++;
+}
